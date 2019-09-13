692f1cad9a907071d62ce0e4b3c94a5c2093d16b
sowjanya ayanampudi <sowjanyabandi@gmail.com>
2019-09-13 14:47:54 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* global EventSource */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import request from "superagent";
+
+import { url } from "./constants";
+
+class App extends React.Component {
+  state = {
+    text: "",
+    messages: []
+  };
+
+  source = new EventSource(`${url}/stream`);
+
+  componentDidMount() {
+    this.source.onmessage = event => {
+      const { data } = event;
+
+      const messages = JSON.parse(data);
+      this.setState({ messages });
+    };
+  }
+
+  onChange = event => {
+    const {
+      target: { value }
+    } = event;
+    console.log("value test:", value);
+
+    this.setState({ text: value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    const { text } = this.state;
+
+    request
+      .post(`${url}/message`)
+      .send({ text })
+      .then(response => {
+        console.log(response);
+        this.setState({ text: "" });
+      })
+      .catch(console.error);
+  };
+
+  onDelete = () => {
+    request
+      .delete(`${url}/message`)
+      .then(console.log)
+      .catch(console.error);
+  };
+
+  render() {
+    const { messages } = this.state;
+
+    console.log("messages:", messages);
+
+    const items = messages.map((message, index) => (
+      <li key={index}>{message.text}</li>
+    ));
+
+    return (
+      <main>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" onChange={this.onChange} value={this.state.text} />
+
+          <button>send</button>
+        </form>
+
+        <button onClick={this.onDelete}>delete</button>
+
+        <ul>{items}</ul>
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const url = "http://localhost:4000";
+//add heroku url below
+//export const url =''
